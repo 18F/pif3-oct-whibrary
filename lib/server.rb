0ba81efa0e8eea55eb45890bff2d23f243a84d66
Robert Sosinski <robert.sosinski@reactive.io>
2014-10-21 20:48:28 +0000

    making better!

--- lib/server.rb
+++ lib/server.rb
@@ -9,7 +9,21 @@ class Server < Sinatra::Base
 
     results = query.dataset.all
 
-    erb :search, locals: {query: query, results: results, q: params[:q]}
+    # binding.pry
+
+    reduced = results.reduce({}) do |memo, item|
+      doctype = item[:doctype]
+
+      if memo[doctype]
+        memo[doctype] << item
+      else
+        memo[doctype] = [item]
+      end
+
+      memo
+    end
+
+    erb :search, locals: {query: query, results: results, q: params[:q], reduced: reduced}
   end
 
   # start the server if ruby file executed directly
